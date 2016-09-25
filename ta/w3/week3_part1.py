4cceb49bdaa7ba94698df9424125e0a2f6db0143
Brian <catzwillruletheearth@gmail.com>
2016-09-25 01:03:36 -0400

    updated python descriptions

---
+++ ta/w3/week3_part1.py
@@ -0,0 +1,13 @@
+x = 7
+y = 10
+x = 8
+x = 10
+x = 8
+x = y
+y = 15
+z = x + y
+print(z)
+x = "Hello"
+y = 5
+#z = x + y <-- explain why this won't work
+print(x * y) #<-- explain how since we didn't save the result of expression, it's now ‘lost'
